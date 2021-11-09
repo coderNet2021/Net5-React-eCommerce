84bb7cff3419f3f491a9804e880e7dd47a1403f2
coderNet2021 <cnss.saida@gmail.com>
2021-11-09 22:26:38 +0200

    initial commit

---
+++ API/Data/Migrations/20211109193254_InitialCreateUpdated.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class InitialCreateUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Picture",
+                table: "Products",
+                newName: "Type");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Products",
+                newName: "Picture");
+        }
+    }
+}
