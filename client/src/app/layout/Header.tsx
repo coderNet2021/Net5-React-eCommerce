0fae37fc286502a7b1182844a69249b1f456fd74
coderNet2021 <cnss.saida@gmail.com>
2021-11-12 13:15:45 +0200

    routing , details view for products

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -8,6 +8,7 @@ import {
   Toolbar,
   Typography,
   Badge,
+  Box,
 } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
@@ -27,48 +28,62 @@ const rightLinks = [
   { title: "register", path: "/register" },
 ];
 
+const navStyles = {
+  color: "inherit",
+  textDecoration: "none",
+  typography: "h6",
+  "&:hover": {
+    color: "grey.500",
+  },
+  "&.active": {
+    color: "text.secondary",
+  },
+};
+
 export default function Header({ darkMode, handleThemeChange }: Props) {
   return (
     <AppBar position="static" sx={{ mb: 4 }}>
-      <Toolbar>
-        <Typography
-          variant="h6"
-          component={NavLink}
-          to="/"
-          sx={{ color: "inherit", textDecoration: "none" }}
-        >
-          RE-STORE
-        </Typography>
-        <Switch checked={darkMode} onChange={handleThemeChange} />
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box display="flex" alignItems="center">
+          <Typography
+            variant="h6"
+            component={NavLink}
+            to="/"
+            exact
+            sx={navStyles}
+          >
+            React.Net-STORE
+          </Typography>
+          <Switch checked={darkMode} onChange={handleThemeChange} />
+        </Box>
+
         <List sx={{ display: "flex" }}>
           {midLinks.map(({ title, path }) => (
-            <ListItem
-              key={path}
-              component={NavLink}
-              to={path}
-              sx={{ color: "inherit", typography: "h6" }}
-            >
+            <ListItem key={path} component={NavLink} to={path} sx={navStyles}>
               {title.toUpperCase()}
             </ListItem>
           ))}
         </List>
-        <IconButton size="large" sx={{ color: "inherit" }}>
-          <Badge badgeContent={4} color="secondary">
-            <ShoppingCart />
-          </Badge>
-        </IconButton>
-        <List sx={{ display: "flex" }}>
-          {rightLinks.map(({ title, path }) => (
-            <ListItem
-              key={path}
-              component={NavLink}
-              to={path}
-              sx={{ color: "inherit", typography: "h6" }}
-            >
-              {title.toUpperCase()}
-            </ListItem>
-          ))}
-        </List>
+        <Box display="flex" alignItems="center">
+          <IconButton size="large" sx={{ color: "inherit" }}>
+            <Badge badgeContent={4} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem key={path} component={NavLink} to={path} sx={navStyles}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Toolbar>
     </AppBar>
   );
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,5 +1,75 @@
-import { Typography } from "@mui/material";
+import {
+  Divider,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Product } from "../../app/models/Product";
 
 export default function ProductDetailsPage() {
-  return <Typography variant="h2">Product Details Page</Typography>;
+  const { id } = useParams<{ id: string }>();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoadnig] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/products/${id}`)
+      .then((response) => setProduct(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoadnig(false));
+  }, [id]);
+
+  if (loading) return <h3>loading ...</h3>;
+  if (!product) return <h3>product not found</h3>;
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={6}>
+        <img
+          src={product.pictureUrl}
+          alt={product.name}
+          style={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <Typography variant="h3">{product.name}</Typography>
+        <Divider sx={{ mb: 2 }}></Divider>
+        <Typography variant="h4" color="secondary">
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <TableContainer>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>{product.name}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>description</TableCell>
+                <TableCell>{product.description}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>type</TableCell>
+                <TableCell>{product.type}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>brand</TableCell>
+                <TableCell>{product.brand}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>quantity in stock</TableCell>
+                <TableCell>{product.quantityInStock}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
+  );
 }
