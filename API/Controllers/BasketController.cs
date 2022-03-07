fe5c74bdad5266c925ab6b1c7bde6a0a56a345bd
Liliane <lliane@Lilianes-MacBook-Pro.local>
2022-03-07 02:24:02 +0100

    first coding ES

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,42 @@
+
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BasketController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket(){
+            var basket= await _context.Baskets
+            .Include(i=>i.Items)
+            .ThenInclude(p=>p.Product)
+            .FirstOrDefaultAsync(x=>x.BuyerId==Request.Cookies["BuyerId"]);
+
+            if(basket==null) return NotFound();
+
+            return basket;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddItemToBasket(int productId, int quantity){
+            return StatusCode(201);
+        }
+
+
+        [HttpDelete]
+        public async Task<ActionResult> AddItemToBasket(){
+            return StatusCode(200);
+        }
+
+    }
+}
